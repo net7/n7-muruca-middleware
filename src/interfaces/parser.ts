f495f10d07edddb3ae643bc2eab8ee9385d8e74e
Duccio <ducciobreschi@gmail.com>
2020-08-04 17:12:37 +0200

    Follow strict types in home parser

--- src/interfaces/parser.ts
+++ src/interfaces/parser.ts
@@ -1,3 +1,13 @@
+export interface Input {
+  /** Array of ordered keys to sort the returned object */
+  keyOrder?: string[];
+  page?: string;
+  /** Data array from the Wordpress endpoint */
+  data: any;
+  /** Configuration object for the parsed layout */
+  conf: any;
+};
+
 export default interface Parser {
-  parse: (input: any) => any;
+  parse: (input: Input) => object;
 }
