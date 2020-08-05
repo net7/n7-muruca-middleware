57aba6427499f354c58ff7d60414ce7879487252
Edgar Gomez <gomez@netseven.it>
2020-08-05 11:22:00 +0200

    updated parser interface

--- src/interfaces/parser.ts
+++ src/interfaces/parser.ts
@@ -1,18 +1,7 @@
 export interface Input {
-  options?: {
-    /** */
-    searchId?: string;
-    /** */
-    type?: string;
-    /** Array of ordered keys to sort the returned object */
-    keyOrder?: string[];
-    /** */
-    page?: string;
-    /** Configuration object for the parsed layout */
-    conf?: any;
-  };
   /** Data array from the Wordpress endpoint */
   data: any;
+  options?: object;
 };
 
 export default interface Parser {
