ab935d1719824a652d8a51f31d39a048aef40056
giacomocerre <cerre.giacomo93@gmail.com>
2020-09-04 12:44:35 +0200

    update dist for refactor search

--- dist/parsers/search.d.ts
+++ dist/parsers/search.d.ts
@@ -1,5 +1,8 @@
 import Parser, { Input } from "../interfaces/parser";
+import { SearchResultsData } from "../interfaces/parser-data/search";
 export declare class SearchParser implements Parser {
     [x: string]: any;
     parse({ data, options }: Input): any;
+    protected parseResults({ data, options }: Input): SearchResultsData;
+    protected parseFacets({ data, options }: Input): any;
 }
--- dist/parsers/search.js
+++ dist/parsers/search.js
@@ -8,5 +8,51 @@ class SearchParser {
             ? this.parseResults({ data, options })
             : this.parseFacets({ data, options });
     }
+    parseResults({ data, options }) {
+        if (options && "limit" in options) {
+            var { offset, limit, sort, total_count } = options;
+        }
+        const search_result = {
+            limit,
+            offset,
+            sort,
+            total_count,
+            results: []
+        };
+        search_result.results = this.parseResultsItems(data, options);
+        return search_result;
+    }
+    parseFacets({ data, options }) {
+        let global_sum = 0;
+        const { facets } = options;
+        const agg_res = {
+            inputs: {
+                total_count: 0,
+                facets: {}
+            }
+        };
+        //header and inputs
+        for (const key in data) {
+            let sum = 0;
+            let values = [];
+            data[key].buckets.map((agg) => {
+                values.push({
+                    text: agg.key,
+                    counter: agg.doc_count,
+                    payload: agg.key
+                });
+                sum = sum + 1;
+            });
+            global_sum = global_sum + sum;
+            const facet = {
+                total_count: sum,
+                values,
+            };
+            agg_res.inputs.facets[key] = facet;
+            agg_res.inputs.total_count = global_sum;
+            console.log(agg_res);
+        }
+        return agg_res;
+    }
 }
 exports.SearchParser = SearchParser;
