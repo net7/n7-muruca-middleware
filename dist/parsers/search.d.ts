d9105e75f2f008d61f7721b61268da5009b30fb0
giacomocerre <cerre.giacomo93@gmail.com>
2020-09-04 11:15:02 +0200

    update dist for bugfix

--- dist/parsers/search.d.ts
+++ dist/parsers/search.d.ts
@@ -1,8 +1,5 @@
 import Parser, { Input } from "../interfaces/parser";
-import { SearchResultsData } from "../interfaces/parser-data/search";
 export declare class SearchParser implements Parser {
     [x: string]: any;
     parse({ data, options }: Input): any;
-    protected parseResults({ data, options }: Input): SearchResultsData;
-    protected parseFacets({ data, options }: Input): any;
 }
--- dist/parsers/search.js
+++ dist/parsers/search.js
@@ -8,51 +8,5 @@ class SearchParser {
             ? this.parseResults({ data, options })
             : this.parseFacets({ data, options });
     }
-    parseResults({ data, options }) {
-        if (options && "limit" in options) {
-            var { limit, offset, sort, total_count } = options;
-        }
-        const search_result = {
-            limit,
-            offset,
-            sort,
-            total_count,
-            results: []
-        };
-        search_result.results = this.parseResultsItems(data, options);
-        //pagination
-        // search_result.results = search_result.results.slice((page - 1) * limit, page * limit)
-        return search_result;
-    }
-    parseFacets({ data, options }) {
-        const { facets } = options;
-        const agg_res = {
-            headers: {},
-            inputs: {}
-        };
-        //header and inputs
-        for (const key in data) {
-            let sum = 0;
-            let inputs = [];
-            data[key].buckets.map((agg) => {
-                inputs.push({
-                    text: agg.key,
-                    counter: agg.doc_count,
-                    payload: agg.key
-                });
-                sum = sum + agg.doc_count;
-            });
-            agg_res.inputs[key] = inputs;
-            agg_res.headers["header-" + key] = sum;
-        }
-        if (facets) {
-            let ordered = {};
-            facets.forEach((key) => {
-                ordered[key] = agg_res.inputs[key] || [];
-            });
-            agg_res.inputs = ordered;
-        }
-        return agg_res;
-    }
 }
 exports.SearchParser = SearchParser;
