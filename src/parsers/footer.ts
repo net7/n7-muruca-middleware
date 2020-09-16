852cc52bc7bd1b2da10af41cb77c1d3ad78f7c65
Duccio <ducciobreschi@gmail.com>
2020-09-16 14:18:01 +0200

    Add poweredby option

--- src/parsers/footer.ts
+++ src/parsers/footer.ts
@@ -1,11 +1,11 @@
 import Parser from "../interfaces/parser";
 
 export class FooterParser implements Parser {
-  parse(data: any, options?: { conf: object }) {
+  parse(data: any, options?: { conf: { poweredby: boolean } }) {
     return {
       columns: [
         ...data,
-        ...options?.conf ? [{
+        ...options?.conf.poweredby ? [{
           classes: 'n7-footer__muruca-promo',
           text: "This project is powered by Muruca, a platform designed and developed by Net7, Pisa, Italy.<br><a href='http://www.muruca.org/' target='_blank'>www.muruca.org</a>, <a href='http://www.netseven.it' target='_blank'>www.netseven.it</a>",
           images: [
