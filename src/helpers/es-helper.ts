7d0068752564a4dba43925fc0325791bce568a0b
Edgar Gomez <gomez@netseven.it>
2020-10-07 10:23:07 +0200

    updated fulltext query key

--- src/helpers/es-helper.ts
+++ src/helpers/es-helper.ts
@@ -95,7 +95,9 @@ export const ESHelper = {
           switch (query_key.type) {
             case "fulltext":
               const ft_query = {
-                multi_match: {
+                // FIXME: controllare ricerca con ideogrammi 
+                // multi_match: {
+                query_string: {
                   query: query_key.addStar ? "*" + data.query + "*" : data,
                   fields: query_key.field
                 }
