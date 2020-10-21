55222e9771bd0a3bfe97fa16a04e3c72ed74f2f9
giacomocerre <cerre.giacomo93@gmail.com>
2020-10-21 11:37:57 +0200

    update build for fix menu lower case

--- src/parsers/menu.ts
+++ src/parsers/menu.ts
@@ -2,7 +2,7 @@ import Parser from "../interfaces/parser";
 
 export class MenuParser implements Parser {
   parse(data: any) {
-    data.map((p:any) => p.slug.toLowerCase())  
+    data.map((p:any) => p.slug = p.slug.toLowerCase())  
     return data;
   }
 }
