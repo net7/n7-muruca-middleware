a7d830984ba40bc83d57dc5f63badabee4ae6c96
Edgar Gomez <gomez@netseven.it>
2020-10-23 10:58:46 +0200

    updated build

--- dist/parsers/translation.js
+++ dist/parsers/translation.js
@@ -2,9 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TranslationParser = void 0;
 class TranslationParser {
-    parse({ data, options }) {
-        let { lang } = options;
-        return { [lang]: data };
+    parse({ data }) {
+        return data;
     }
 }
 exports.TranslationParser = TranslationParser;
